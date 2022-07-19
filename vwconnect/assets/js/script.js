38d02f8399d8369c140e21442321b3df5117d2ac
unknown <Sambhu.Sharma.Metacube@BeamSuntory.com>
2022-07-19 18:55:54 +0530

    create new tabs Panel and Trench in lots page

--- vwconnect/assets/js/script.js
+++ vwconnect/assets/js/script.js
@@ -1,7 +1,26 @@
-
-$("#menu-button").click(function(){
+$(document).ready(function () {
+  //menu-button for sidebar-toggle
+  $("#menu-button").click(function () {
     $("#sidebar-toggle").fadeToggle();
   });
 
- $('#table_id').DataTable({
+  //DataTable
+  $("#table_id").DataTable({});
+
+  //Remove danger btn
+  $(".lots-action .btn-danger").hide();
+
+  $(".lots-tabs .nav-item").on("click", function () {
+    var target = $(this).find("#production-tab").attr("id");
+    var contentTarget = $(this)
+      .parents(".lots-tabs")
+      .find(".tab-content #production")
+      .attr("aria-labelledby");
+
+    if (target != contentTarget) {
+      $(".lots-action .btn-danger").hide();
+    } else {
+      $(".lots-action .btn-danger").show();
+    }
   });
+});
